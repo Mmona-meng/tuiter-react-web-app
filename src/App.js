1782bd5f93fbf7928aea86cbc39d84617f72a0d6
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2022-11-01 01:39:07 -0700

    a6 lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Labs from "./labs/index.js";
+import HelloWorld from "./labs/a6/hello-world.js";
+import Tuiter from "./tuiter/index.js";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is branch a2.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="container">
+            <HelloWorld/>
+            <Labs/>
+            <Tuiter/>
+          </div>
+      </BrowserRouter>
   );
 }
-
 export default App;
