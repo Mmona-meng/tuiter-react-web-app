315b7f8efb1cf02dac69adea62995897202f6f3a
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2022-11-06 17:56:14 -0800

    a7

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,11 +1,16 @@
-import Assignment6 from "./a6/index.js";
 import Nav from "../nav.js";
+import Assignment6 from "./a6/index.js";
+import Assignment7 from "./a7/index.js";
+import {Routes, Route} from "react-router";
 
 function Labs() {
     return(
         <div>
             <Nav/>
-            <Assignment6/>
+            <Routes>
+                <Route index element={<Assignment6/>}/>
+                <Route path="a7" element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
